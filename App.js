806d2ae4104ae3592b540b5ba8edb9d69c1354ad
Vinicius Silva Souza <viniciusvss120@gmail.com>
2024-01-31 17:24:32 -0400

    Aprendendo a utilizar a camera no React-Native

--- App.js
+++ App.js
@@ -1,20 +1,143 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image } from 'react-native';
+import {Camera} from 'expo-camera'
+import {FontAwesome} from '@expo/vector-icons' 
 
 export default function App() {
+  const camRef = useRef(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission, setHasPermission] = useState(null)
+  const [capturedPhoto, setCapturedPhoto] = useState(null)
+  const [open, setOpen] = useState(false)
+
+  const permissionFunction = async () => {
+    const {status} = await Camera.requestCameraPermissionsAsync()
+    setHasPermission(status === 'granted')
+  }
+  useEffect(() => {
+    permissionFunction()
+  }, [])
+
+  if(hasPermission === null) {
+    return <Text>Deu Ruim</Text>
+  }
+
+  if(hasPermission === false) {
+    return <Text>Acesso Negado</Text>
+  }
+
+  async function takePicture() {
+    if(camRef){
+      const data = await camRef.current.takePictureAsync()
+      setCapturedPhoto(data.uri)
+      setOpen(true)
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={camRef}
+      >
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                ?
+                Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              )
+            }}
+          >
+            <FontAwesome name='exchange' size={23} color="red"></FontAwesome>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonCamera}
+            onPress={takePicture}
+          >
+            <FontAwesome name='camera' size={23} color="#fff"></FontAwesome>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {capturedPhoto && (
+        <Modal
+          animationType='slide'
+          transparent={true}
+          visible={open}
+        >
+          <View style={styles.contentModal}>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setOpen(false)}
+            >
+              <FontAwesome name='close' size={50} color="#fff"></FontAwesome>
+            </TouchableOpacity>
+            <Image 
+              style={styles.imgPhoto}
+              source={{uri: capturedPhoto}}
+            />
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    width: "100%",
+    height: "100%"
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row"
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50
+  },
+  buttonCamera: {
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50
+  },
+  contentModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    margin: 20
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    left: 2,
+    margin: 10
+  },
+  imgPhoto: {
+    width: "100%",
+    height: 400
+  }
 });
